99f908e015fbe7be47ab7ab1aa0607c0044a0a50
jonathan <koren@pukogames.com>
2021-10-21 13:45:23 +0900

    Remove refreshCookieDomain

--- refresher.go
+++ refresher.go
@@ -21,7 +21,6 @@ type RefresherConfig struct {
 	RefreshURI               string `json:"refresh_url"`
 	RefreshBodyProperty      string `json:"refresh_property"`
 	RefreshCookieKey         string `json:"refresh_cookie_key"`
-	RefreshCookieDomain      string `json:"refresh_cookie_domain"`
 }
 
 func NewRefresher(cfg *config.EndpointConfig) (Refresher, error) {
